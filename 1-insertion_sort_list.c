7fd720e227886e81dc117e0fd5ac2e66caea01ac
davud <davudgeroqiq@gmail.com>
2026-02-07 23:32:39 +0300

    new commit

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+include "sort.h"
+/**
+ * insertion_sort_list - sorts a list using insertion algorithm
+ * @list: the list
+ *
+ * Return: nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *tmp, *current, *prv;
+	if (**list != NULL && *list != NULL)
+	{
+		current = (*list)->next;
+		while (current)
+		{
+			prev = current->prev;
+			while (prev)
+			{
+				if (current->n < prev->n)
+				{
+					prev->next = current->next;
+					if (current->next != NULL)
+						current->next->prev = prev;
+
+					current->prev = prev->prev;
+					current->next = prev;
+
+					if (prev->prev)
+						prev->prev->next = current;
+					else
+						*list = current;
+					prev->prev = current;
+					prev = current->prev;
+					print_list(*list);
+				}
+				else
+					break;
+			}
+			current = prev->next;
+		}
+	}
+}
