b9fcc771b6ef9125d852bb76af6edae2b02b2303
davud <davudgeroqiq@gmail.com>
2026-02-07 23:59:50 +0300

    new commit

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts an array using selection sort algorithm
+ * @array: array
+ * @size: size of the array
+ *
+ * Return: nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	int min_index = 0;
+	int current = 0;
+	int i, k;
+	for (i = 0; i < size; i++)
+	{
+		for (k = current + 1; k < size; k++)
+		{
+			if (array[k] < array[min_index])
+			{
+				min_index = k;
+			}
+		}
+		if (min_index != current)
+		{
+			k = array[min_index];
+			array[min_index] = array[current];
+			array[current] = k;
+			print_array(array, size);
+		}
+		current++;
+		min_index = current;
+	}
+}
+		
